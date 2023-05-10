b8432f634fb61f05eb4c0e551332a10e795ff1a2
Mohamed Ahmed Slama <126114667+Slama305@users.noreply.github.com>
2023-05-10 10:54:57 +0300

    Add files via upload

--- code_slama.cpp
+++ code_slama.cpp
@@ -10,25 +10,28 @@ using namespace std;
 #define db double
 #define ft float
 #define nl cout<<"\n";
+#define spa cout<<" ";
 #define sp(x) setprecision(x)
 #define u_ll unsigned long long
 #define u_int unsigned int
-#define sz(s) long long(s.size())
+#define sz(s) s.size()
 #define for0(i,y,x) for(int i=y;i<x;i++)
 #define for_(i,x,y) for(int i=x;i>=y;i--)
 /*_________scan___________*/
 #define cini(x) int x;cin>>x;
-#define cinf(x) flaot x;cin>>x;
+#define cini2(x,y) int x,y;cin>>x>>y;
+#define cinf(x) float x;cin>>x;
 #define cins(x) string x;cin>>x;
 #define cinc(x) char x;cin>>x;
 #define cind(x) double x;cin>>x;
 #define cinl(x) long long x;cin>>x;
 #define cinl2(x,y) long long x,y;cin>>x>>y;
-#define cin_arr[x] int arr[x];for(int i=0;i<x;i++)cin>>arr[i];
-#define cin_arr[x][y] int arr[x][y];for(int i=0;i<x;i++){for(int j=0;j<x;j++)cin>>arr[i][j];}
+#define cin_arr(arr,x) ll arr[x];for(int i=0;i<x;i++)cin>>arr[i];
+#define cin_arr2(arr,x,y) int arr[x][y];for(int i=0;i<x;i++){for(int j=0;j<y;j++)cin>>arr[i][j];}
 /*_________print___________*/
 #define pr1(x) cout<<x
 #define pr2(x,y) cout<<x<<" "<<y
+#define pri_arr(x) int arr[x];for(int i=0;i<x;i++)cout<<arr[i]<<" ";
 /*___*****yes & no*****___*/
 #define YES cout<<"YES\n"
 #define Yes cout<<"Yes\n"
@@ -64,13 +67,15 @@ using namespace std;
   ________________________________________________________________________
   ________________________________________________________________________
 */
+void rec(int num){
+	
+}
 void solve(){//code solve ?
    int t=1;
 	//cin>>t;
 	while(t--){// test case ?
-
-
-
+    cini(n);
+    rec(n);
 	}
 }
 int main(int argc, char** argv) {
