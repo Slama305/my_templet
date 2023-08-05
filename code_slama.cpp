4d9702df511da8bfeedfc6ee219c73e4e6f1b70d
Mohamed Ahmed Slama <126114667+Slama305@users.noreply.github.com>
2023-08-05 23:16:33 +0300

    Update code_slama.cpp

--- code_slama.cpp
+++ code_slama.cpp
@@ -1,7 +1,7 @@
-             /*     بسم الله الرحمن الرحيم       */
-             /*    لا اله الا الله محمد رسول الله   */
-             /*   اللهم لا سهل الا ما جعلته سهلا   */
-               /*________ACCEPT________*/
+             /* بسم الله الرحمن الرحيم */
+          /* لا اله الا الله محمد رسول الله  */
+          /* اللهم لا سهل الا ما جعلته سهلا */
+          /*________ACCEPT________*/
 #include <bits/stdc++.h>
 using namespace std;
 #define slama() ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
@@ -20,18 +20,19 @@ using namespace std;
 /*_________scan___________*/
 #define cini(x) int x;cin>>x;
 #define cini2(x,y) int x,y;cin>>x>>y;
+#define cini3(x,y,z) int x,y,z;cin>>x>>y>>z;
 #define cinf(x) float x;cin>>x;
+#define cinS(x) string x;getline(cin,x);
 #define cins(x) string x;cin>>x;
 #define cinc(x) char x;cin>>x;
 #define cind(x) double x;cin>>x;
 #define cinl(x) long long x;cin>>x;
 #define cinl2(x,y) long long x,y;cin>>x>>y;
-#define cin_arr(arr,x) ll arr[x];for(int i=0;i<x;i++)cin>>arr[i];
-#define cin_arr2(arr,x,y) int arr[x][y];for(int i=0;i<x;i++){for(int j=0;j<y;j++)cin>>arr[i][j];}
+#define cinl3(x,y,z) long long x,y,z;cin>>x>>y>>z;
 /*_________print___________*/
 #define pr1(x) cout<<x
 #define pr2(x,y) cout<<x<<" "<<y
-#define pri_arr(arr,x) int arr[x];for(int i=0;i<x;i++)cout<<arr[i]<<" ";
+#define pri_arr(x) int arr[x];for(int i=0;i<x;i++)cout<<arr[i]<<" ";
 /*___*****yes & no*****___*/
 #define YES cout<<"YES\n"
 #define Yes cout<<"Yes\n"
@@ -41,17 +42,6 @@ using namespace std;
 #define no cout<<"no\n"
 /*___________________________الحمد لله__________________________________*/
 /*
-                                                            *   *
-  **            **                                         *** ***    ** **
-  **            **                                          *   *     ** **
-  **            **                                            ***     ** **
-  **  *         **                                   *       *****    ** **
-  ** ***        **               **   **  **         **     **   **   ** **
-  **  *    **   **     **        **   **  **         **     **   **   ** **
-  **      **   **    **         **   **  **  *      **      ** **    ** **
-  **************    **         ***********  ************************** **
-   ************      ********************    ************************  *
-                      
   ________________________________________________________________________
   ________________________________________________________________________
 
@@ -67,15 +57,90 @@ using namespace std;
   ________________________________________________________________________
   ________________________________________________________________________
 */
-void rec(int num){
-	
-}
+/*___*****STLs*****___*/
+   /***__vector__***/ 
+   // -> dynamic array
+   // -> vector<data type> v1;
+   // -> vector<data type> v2(size,0);
+   // -> vector<data type> v3(v2) copy element
+   //function 
+   /*
+   vector<int>v1;
+   v1.push_back(1);//insert one elemente from back
+   v1.pop_back();//delete one elemente
+   v1.clear(); // delete all elemente
+  _______________map_______________________
+  map<key ,map value> nameMap;
+  erase(iterator) delete element  -> erase(ftom,to)
+  ______________set______________________
+  associative containers 
+  binry treea
+  sorted
+  fast
+  unique value
+  no push_back , pop
+  random access not supported
+  min value = set.begin()
+  max  //   = set.end()
+  multiset more same value 
+   
+   */
+#define pb push_back  //
+#define pf push_front
+#define ppb pop_back
+#define IT iterator 
+#define ff first
+#define ss second
+#define del erase 
+#define all(s) s.begin(),s.end()
+#define alla(arr ,s) arr , arr + s
+#define m_p make_pair
+#define TC(t) while(t--)
+#define lb lower_bound
+#define ub upper_bound
+//#define
+#define V(n,sz) vector<int>n(sz);
+#define add_vector(v,n)  for(int i=0;i<n;i++){cin >> v[i] ;}
+template<typename T>
+void swap(T& a,T& b){int temp=a;a=b;b=temp;}
+void rec(int num){}
+ll gcd(ll a,ll b){return b==0 ? a : gcd(b,a%b);}
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
 void solve(){//code solve ?
-   int t=1;
-	//cin>>t;
-	while(t--){// test case ?
-    cini(n);
-    rec(n);
+   int T=1;
+	//cin>>T;
+   TC(T){// test case ?
+    cini(n); 
+
+
+
+
+
+
+
+
+
+
+
+
+    
+////////////////////////////////////////////////////// 
+//  map<int ,string>mp;
+//        while(n--){
+//    	    cini(num);
+//    	    cins(str);
+//        	mp.insert({num , str});
+//    	}  
+//    for(auto it:mp)cout<<it.ff<<" "<<it.ss<<endl;
+//    
+//    
+//    V(v,n)     // name , size 
+//    add_vector(v,n);
+//    v.p_b(n);
+//    v.insert(0,6);
+//	 for( auto it : v ){cout << it << " " ;}
+   // swap<int>(a,b);
+   // rec(n);
 	}
 }
 int main(int argc, char** argv) {
